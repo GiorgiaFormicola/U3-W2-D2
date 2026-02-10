b99fd3f4b3f34c566ac34f5655d2bfc33e5046ab
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-02-10 13:58:38 +0100

    SingleComment updated to function component

--- epibooks-project/src/components/SingleComment.jsx
+++ epibooks-project/src/components/SingleComment.jsx
@@ -2,18 +2,16 @@ import { Component } from "react";
 import ListGroup from "react-bootstrap/ListGroup";
 import "bootstrap-icons/font/bootstrap-icons.min.css";
 
-class SingleComment extends Component {
-  render() {
-    return (
-      <ListGroup.Item className="d-flex justify-content-between px-2  align-items-baseline">
-        <span className="fst-italic">"{this.props.commentText}"</span>
-        <small className="text-secondary d-flex align-items-center gap-1">
-          <span>{this.props.commentRate}/5</span>
-          <i className="bi bi-star-fill" style={{ fontSize: "0.8em" }}></i>
-        </small>
-      </ListGroup.Item>
-    );
-  }
-}
+const SingleComment = function (props) {
+  return (
+    <ListGroup.Item className="d-flex justify-content-between px-2  align-items-baseline">
+      <span className="fst-italic">"{props.commentText}"</span>
+      <small className="text-secondary d-flex align-items-center gap-1">
+        <span>{props.commentRate}/5</span>
+        <i className="bi bi-star-fill" style={{ fontSize: "0.8em" }}></i>
+      </small>
+    </ListGroup.Item>
+  );
+};
 
 export default SingleComment;
