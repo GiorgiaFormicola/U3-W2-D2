dbf228e54a40a548c062ed71c77d13b15856d6f5
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-02-10 14:13:07 +0100

    CommentArea updated to function component

--- epibooks-project/src/components/CommentArea.jsx
+++ epibooks-project/src/components/CommentArea.jsx
@@ -1,17 +1,16 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import CommentList from "./CommentList";
 import AddComment from "./AddComment";
 
 const URL = "https://striveschool-api.herokuapp.com/api/comments/";
 const key =
   "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OTczM2Q1ODg1ZTNiMTAwMTViNWVkOTIiLCJpYXQiOjE3NzA2NDM4NTMsImV4cCI6MTc3MTg1MzQ1M30.Cm9gOiPR0KyOzR70lK_Fl6k-6KIZR3NS3RaJU7DRle0";
-class CommentArea extends Component {
-  state = {
-    commentsArray: [],
-  };
 
-  getComments = () => {
-    fetch(URL + this.props.bookID, {
+const CommentArea = function (props) {
+  const [commentsArray, setCommentsArray] = useState([]);
+
+  const getComments = () => {
+    fetch(URL + props.bookID, {
       headers: {
         Authorization: key,
       },
@@ -24,39 +23,31 @@ class CommentArea extends Component {
         }
       })
       .then((bookCommentsArray) => {
-        this.setState({
-          commentsArray: bookCommentsArray,
-        });
+        setCommentsArray(bookCommentsArray);
       })
       .catch((err) => {
         console.log("ERROR", err);
       });
   };
-  componentDidMount() {
-    this.getComments();
-  }
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.bookID !== this.props.bookID) {
-      this.getComments();
-    }
-  }
-  render() {
-    return (
-      <>
-        {!this.props.bookID && <h4>Select a book to leave a comment or read its reviews!</h4>}
-        {this.props.bookID && (
-          <>
-            <h4 className="text-light text-center pt-3" style={{ marginTop: "0.55em", marginBottom: "1.25em" }}>
-              {this.props.bookTitle}
-            </h4>
-            <CommentList commentsArray={this.state.commentsArray}></CommentList>
-            <AddComment bookID={this.props.bookID} getComments={this.getComments}></AddComment>
-          </>
-        )}
-      </>
-    );
-  }
-}
+  useEffect(() => {
+    getComments();
+  }, [props.bookID]);
+
+  return (
+    <>
+      {!props.bookID && <h4>Select a book to leave a comment or read its reviews!</h4>}
+      {props.bookID && (
+        <>
+          <h4 className="text-light text-center pt-3" style={{ marginTop: "0.55em", marginBottom: "1.25em" }}>
+            {props.bookTitle}
+          </h4>
+          <CommentList commentsArray={commentsArray}></CommentList>
+          <AddComment bookID={props.bookID} getComments={getComments}></AddComment>
+        </>
+      )}
+    </>
+  );
+};
 
 export default CommentArea;
